b5cca235494db079796fbd0295358e2a9e7cc415
Mohsin Shaikh <mohsinshaikh78601@gmail.com>
2023-04-11 17:19:29 +0530

    Button variant created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,15 @@ import Button from '@/components/button'
 export default function Home() {
     return (<div className='container mx-auto'>
         <h1>Design System</h1>
-        <Button label='Submit' onClick={() => alert()} />
+        <Button 
+            label='Submit' 
+            onClick={() => alert()} 
+            variant={'primary'}
+        />
+        <Button 
+            label='Submit' 
+            onClick={() => alert()} 
+            variant={'secondary'}
+        />
     </div>)
 }
