91f77de7644cb0d2b9def0e126b2001da5c8bf1f
Mohsin Shaikh <mohsinshaikh78601@gmail.com>
2023-05-12 01:27:56 +0530

    new way to develop UI components

---
+++ src/pages/ui/button.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/Button";
+
+
+const ButtonPage = () => {
+    return (<div className='container mx-auto my-2 space-y-4'>
+        <Button>👍 Button</Button>
+    </div>);
+}
+
+export default ButtonPage;
